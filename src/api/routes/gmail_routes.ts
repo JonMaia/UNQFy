fc2e4898fe81ab4e587151842191479ec18765e9
jonmaia <jonathannicolas.maia@gmail.com>
2019-07-05 19:36:36 -0300

    Se crearon las rutas de Gmail

---
+++ src/api/routes/gmail_routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { GmailController } from "../../api_gmail/gmail_controller";
+
+
+const router = Router();
+
+router.route('/subscribe')
+      .post(GmailController.validateData.bind(GmailController), GmailController.suscribe.bind(GmailController));
+
+router.route('/unsubscribe')
+      .post(GmailController.validateData.bind(GmailController), GmailController.unsubscribe.bind(GmailController));
+
+router.route('/notify')
+      .post(GmailController.validateData.bind(GmailController), GmailController.notify.bind(GmailController));
+
+router.route('/subscriptions?artistId=:id')
+      .get(GmailController.validateData.bind(GmailController), GmailController.subscriptions.bind(GmailController));
+
+router.route('/subscriptions')
+      .delete(GmailController.validateData.bind(GmailController), GmailController.deleteSubscriptions.bind(GmailController));
+
+export default router;
--- src/init_spotify_auth.ts
+++ src/init_spotify_auth.ts
@@ -2,7 +2,6 @@ import { SpotifyService } from "./api_spotify/spotify_service";
 
 function main() {
     SpotifyService.authenticate();
-    
 }
 
 main();
