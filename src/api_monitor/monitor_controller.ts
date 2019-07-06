ea210c1077cf8ded3c1482223e9df39168d6b735
Ariel Ramirez <arielramirez20395@gmail.com>
2019-07-06 20:36:49 -0300

    Se extiende la verificación de status de los servidores
    * Utilizamos net para realizar la comprobación de si esta activo o no el servidor
    * Como siguiente paso los servidores deben ser leídos de un archivo de texto, ya que si usamos docker cada servidor estará en distintos
    hostings

--- src/api_monitor/monitor_controller.ts
+++ src/api_monitor/monitor_controller.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express";
+import net from "net";
 
 export class MonitorController {
 
     private static enabled: boolean = true;
+    private static readonly SERVERS = [
+        {name_server: 'Monitor', hostname: 'localhost', port: 5000},
+        {name_server: 'UnqFy', hostname: 'localhost', port: 3000}
+    ]
 
     public static activate(req: Request, res: Response): Response {
         MonitorController.enabled = true;
@@ -14,10 +19,56 @@ export class MonitorController {
         return res.status(200).json();
     }
 
-    public static status(req: Request, res: Response): Response {
-        return res.status(200).json({
-            service: 'Monitor',
-            status: (MonitorController.enabled ? 'Online' : 'Offline')
+    public static status(req: Request, res: Response): Response | undefined {
+        if(!MonitorController.enabled) {
+            return res.status(200).json({
+                service: 'Monitor',
+                status: 'Offline'
+            });
+        } else {
+            Promise.all(MonitorController.SERVERS.map((server) => {
+                return this.checkConnection(server.hostname, server.port)
+                        .then(() => {
+                            return true;
+                        })
+                        .catch((err) => {
+                            if(err.code) {
+                                console.log(`${err.code}: '${err.address}:${err.port}'`)
+                            } else {
+                                console.log(err);
+                            }
+                            return false;
+                        })
+                        .then((isOnline: boolean) => {
+                            return {
+                                service: server.name_server,
+                                status: (isOnline ? 'Online' : 'Offline')
+                            }
+                        });
+            }))
+            .then((serverStatus) => {
+                return res.status(200).json(serverStatus);
+            })
+        }
+    }
+
+    private static checkConnection(host: string, port: number, timeout?: number) {
+        return new Promise((resolve, reject) => {
+            timeout = timeout || 10000;     // default of 10 seconds
+            let timer = setTimeout(() => {
+                reject("timeout");
+                socket.end();
+            }, timeout);
+            let socket = net.createConnection(port, host, () => {
+                clearTimeout(timer);
+                resolve();
+                socket.end();
+            });
+            socket.on('error', (err) => {
+                clearTimeout(timer);
+                reject(err);
+                socket.end();
+            });
         });
     }
 }
